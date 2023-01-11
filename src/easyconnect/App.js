d9cee3c5fe689344f996637304c6a95ffa23749c
ajaypoly <ajaypoly99@gmail.com>
2023-01-11 11:33:32 +0530

    Easy Connect

---
+++ src/easyconnect/App.js
@@ -0,0 +1,185 @@
+import React, { useCallback, useRef } from "react";
+
+import ReactFlow, {
+  addEdge,
+  MarkerType,
+  ConnectionLineType,
+  useNodesState,
+  useEdgesState,
+  updateEdge,
+} from "reactflow";
+
+import CustomNode from "./CustomNode";
+import FloatingEdge from "./FloatingEdje";
+import CustomConnectionLine from "./CustomConnectionLine";
+import dagre from "dagre";
+import "reactflow/dist/style.css";
+import "./style.css";
+const dagreGraph = new dagre.graphlib.Graph();
+dagreGraph.setDefaultEdgeLabel(() => ({}));
+const nodeWidth = 172;
+const nodeHeight = 36;
+const initialNodes = [
+  {
+    id: "1",
+    type: "custom",
+  },
+  {
+    id: "2",
+    type: "custom",
+  },
+  {
+    id: "3",
+    type: "custom",
+  },
+  {
+    id: "4",
+    type: "custom",
+  },
+];
+
+const initialEdges = [
+    // {
+    //     id: 'edge-1-2',
+    //     source: '1',
+    //     target: '2',
+    //     type: 'buttonedge',
+    //   },
+    //   {
+    //     id: 'edge-1-2',
+    //     source: '2',
+    //     target: '4',
+    //     type: 'buttonedge',
+    //   },
+    //   {
+    //     id: 'edge-1-2',
+    //     source: '2',
+    //     target: '3',
+    //     type: 'buttonedge',
+    //   },
+    //   {
+    //     id: 'edge-1-2',
+    //     source: '1',
+    //     target: '4',
+    //     type: 'buttonedge',
+    //   },
+];
+
+const connectionLineStyle = {
+  strokeWidth: 1,
+  stroke: "black",
+};
+
+const nodeTypes = {
+  custom: CustomNode,
+};
+
+const edgeTypes = {
+  floating: FloatingEdge,
+};
+
+const getLayoutedElements = (nodes, edges, direction = "LR") => {
+  const isHorizontal = direction === "LR";
+  dagreGraph.setGraph({ rankdir: direction });
+
+  nodes.forEach((node) => {
+    dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight });
+  });
+
+  edges.forEach((edge) => {
+    dagreGraph.setEdge(edge.source, edge.target);
+  });
+
+  dagre.layout(dagreGraph);
+
+  nodes.forEach((node) => {
+    const nodeWithPosition = dagreGraph.node(node.id);
+    node.targetPosition = isHorizontal ? "left" : "top";
+    node.sourcePosition = isHorizontal ? "right" : "bottom";
+
+    // We are shifting the dagre node position (anchor=center center) to the top left
+    // so it matches the React Flow node anchor point (top left).
+    node.position = {
+      x: nodeWithPosition.x - nodeWidth / 2,
+      y: nodeWithPosition.y - nodeHeight / 2,
+    };
+
+    return node;
+  });
+
+  return { nodes, edges };
+};
+
+const defaultEdgeOptions = {
+  style: { strokeWidth: 1, stroke: "black" },
+  type: "floating",
+  markerEnd: {
+    type: MarkerType.ArrowClosed,
+    color: "black",
+  },
+};
+const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(
+  initialNodes,
+  initialEdges
+);
+
+const EasyConnectExample = () => {
+  const edgeUpdateSuccessful = useRef(true);
+
+  const [nodes, setNodes, onNodesChange] = useNodesState(layoutedNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(layoutedEdges);
+  console.log(edges);
+
+  const onConnect = useCallback(
+    (params) =>
+      setEdges((eds) =>
+        addEdge(
+          { ...params, type: ConnectionLineType.SmoothStep, animated: true },
+          eds
+        )
+      ),
+    []
+  );
+
+  const onEdgeUpdateStart = useCallback(() => {
+    edgeUpdateSuccessful.current = false;
+  }, []);
+
+  const onEdgeUpdate = useCallback((oldEdge, newConnection) => {
+    edgeUpdateSuccessful.current = true;
+    setEdges((els) => updateEdge(oldEdge, newConnection, els));
+  }, []);
+
+  const onEdgeUpdateEnd = useCallback((_, edge) => {
+    if (!edgeUpdateSuccessful.current) {
+      setEdges((eds) => eds.filter((e) => e.id !== edge.id));
+    }
+
+    edgeUpdateSuccessful.current = true;
+  }, []);
+
+  return (
+    <div className="layoutflow" style={{ width: "100%", height: "100vh" }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+        nodeTypes={nodeTypes}
+        edgeTypes={edgeTypes}
+        defaultEdgeOptions={defaultEdgeOptions}
+        connectionLineComponent={CustomConnectionLine}
+        connectionLineStyle={connectionLineStyle}
+        snapToGrid
+        onEdgeUpdate={onEdgeUpdate}
+        onEdgeUpdateStart={onEdgeUpdateStart}
+        onEdgeUpdateEnd={onEdgeUpdateEnd}
+        connectionLineType={ConnectionLineType.SmoothStep}
+      />
+    </div>
+  );
+};
+
+export default EasyConnectExample;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Flow from './App';
+import EasyConnectExample from './easyconnect/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Flow/>
+    {/* <Flow/> */}
+    <EasyConnectExample/>
   </React.StrictMode>
 );
 
