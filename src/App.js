b988b66d23849afe34ed29cf6a08fb4f64b4e736
muhammedshamilv <shamilrjv@gmail.com>
2023-01-21 10:37:04 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,112 +1,225 @@
-// import React, { useCallback, useRef } from "react";
-// import ReactFlow, {
-//   addEdge,
-//   ConnectionLineType,
-//   useNodesState,
-//   useEdgesState,
-//   updateEdge,
-// } from "reactflow";
-// import dagre from "dagre";
-// import "reactflow/dist/style.css";
+import React, { useCallback, useRef, useState } from 'react';
 
-// import { initialNodes, initialEdges } from "./nodes-edges.js";
+import ReactFlow, {
+  addEdge,
+  MarkerType,
+  ConnectionLineType,
+  useNodesState,
+  useEdgesState,
+  updateEdge
+} from 'reactflow';
 
-// import "./Index.css";
+import CustomNode from './CustomNode';
+import CustomConnectionLine from './CustomConnectionLine';
+import dagre from 'dagre';
+import { useStore, getStraightPath } from 'reactflow';
+import './style.css';
+import { getEdgeParams } from './utils.js';
+import 'reactflow/dist/style.css';
+import './style.css';
 
-// const dagreGraph = new dagre.graphlib.Graph();
-// dagreGraph.setDefaultEdgeLabel(() => ({}));
+const dagreGraph = new dagre.graphlib.Graph();
+dagreGraph.setDefaultEdgeLabel(() => ({}));
+const nodeWidth = 172;
+const nodeHeight = 36;
 
-// const nodeWidth = 172;
-// const nodeHeight = 36;
+const initialNodes = [
+  {
+    id: 'task 0381c7c2-851d-4110-937e-b7c464c2787f',
+    type: 'custom',
+    data: { label: 'Node B' }
+  },
+  {
+    id: '2',
+    type: 'custom'
+  },
+  {
+    id: '3',
+    type: 'custom'
+  },
+  {
+    id: '4',
+    type: 'custom'
+  }
+];
+const initialEdges = [
+  // { id: 'e1-2', source: '1', target: '2',}
+];
 
-// const getLayoutedElements = (nodes, edges, direction = "LR") => {
-//   const isHorizontal = direction === "LR";
-//   dagreGraph.setGraph({ rankdir: direction });
+const connectionLineStyle = {
+  strokeWidth: 1,
+  stroke: 'black'
+};
 
-//   nodes.forEach((node) => {
-//     dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight });
-//   });
+const nodeTypes = {
+  custom: CustomNode
+};
 
-//   edges.forEach((edge) => {
-//     dagreGraph.setEdge(edge.source, edge.target);
-//   });
+const getLayoutedElements = (nodes, edges, direction = 'LR') => {
+  const isHorizontal = direction === 'LR';
+  dagreGraph.setGraph({ rankdir: direction });
 
-//   dagre.layout(dagreGraph);
+  nodes.forEach((node) => {
+    dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight });
+  });
 
-//   nodes.forEach((node) => {
-//     const nodeWithPosition = dagreGraph.node(node.id);
-//     node.targetPosition = isHorizontal ? "left" : "top";
-//     node.sourcePosition = isHorizontal ? "right" : "bottom";
+  edges.forEach((edge) => {
+    dagreGraph.setEdge(edge.source, edge.target);
+  });
 
-//     // We are shifting the dagre node position (anchor=center center) to the top left
-//     // so it matches the React Flow node anchor point (top left).
-//     node.position = {
-//       x: nodeWithPosition.x - nodeWidth / 2,
-//       y: nodeWithPosition.y - nodeHeight / 2,
-//     };
+  dagre.layout(dagreGraph);
 
-//     return node;
-//   });
+  nodes.forEach((node) => {
+    const nodeWithPosition = dagreGraph.node(node.id);
+    node.targetPosition = isHorizontal ? 'left' : 'top';
+    node.sourcePosition = isHorizontal ? 'right' : 'bottom';
 
-//   return { nodes, edges };
-// };
+    // We are shifting the dagre node position (anchor=center center) to the top left
+    // so it matches the React Flow node anchor point (top left).
+    node.position = {
+      x: nodeWithPosition.x - nodeWidth / 2,
+      y: nodeWithPosition.y - nodeHeight / 2
+    };
 
-// const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(
-//   initialNodes,
-//   initialEdges
-// );
+    return node;
+  });
 
-// const LayoutFlow = () => {
-//   const edgeUpdateSuccessful = useRef(true);
-//   const [nodes, setNodes, onNodesChange] = useNodesState(layoutedNodes);
-//   const [edges, setEdges, onEdgesChange] = useEdgesState(layoutedEdges);
-//   console.log("",edges);
+  return { nodes, edges };
+};
 
-//   const onConnect = useCallback(
-//     (params) =>
-//       setEdges((eds) =>
-//         addEdge(
-//           { ...params, type: ConnectionLineType.SmoothStep, animated: true },
-//           eds
-//         )
-//       ),
-//     []
-//   );
+const defaultEdgeOptions = {
+  style: { strokeWidth: 1, stroke: 'black' },
+  type: 'floating',
+  label: 'x',
 
-//   const onEdgeUpdateStart = useCallback(() => {
-//     edgeUpdateSuccessful.current = false;
-//   }, []);
+  markerEnd: {
+    type: MarkerType.ArrowClosed,
+    color: 'black'
+  }
+};
+const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(
+  initialNodes,
+  initialEdges
+);
 
-//   const onEdgeUpdate = useCallback((oldEdge, newConnection) => {
-//     edgeUpdateSuccessful.current = true;
-//     setEdges((els) => updateEdge(oldEdge, newConnection, els));
-//   }, []);
+const EasyConnectExample = () => {
+  const [nodes, setNodes, onNodesChange] = useNodesState(layoutedNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(layoutedEdges);
+  console.log(edges);
+  const edgeUpdateSuccessful = useRef(true);
 
-//   const onEdgeUpdateEnd = useCallback((_, edge) => {
-//     if (!edgeUpdateSuccessful.current) {
-//       setEdges((eds) => eds.filter((e) => e.id !== edge.id));
-//     }
+  // const onConnect = useCallback(
+  //   (params) => setEdges((eds) => addEdge(params, eds)),
+  //   [setEdges]
+  // );
 
-//     edgeUpdateSuccessful.current = true;
-//   }, []);
+  const onConnect = useCallback(
+    (params) => setEdges((eds) => addEdge({ ...params, label: 'x' }, eds)),
+    [setEdges]
+  );
+  const onEdgeUpdateStart = useCallback(() => {
+    edgeUpdateSuccessful.current = false;
+  }, []);
 
-//   return (
-//     <div className="layoutflow" style={{ width: "100%", height: "100vh" }}>
-//       <ReactFlow
-//         nodes={nodes}
-//         edges={edges}
-//         onNodesChange={onNodesChange}
-//         onEdgesChange={onEdgesChange}
-//         onConnect={onConnect}
-//         snapToGrid
-//         onEdgeUpdate={onEdgeUpdate}
-//         onEdgeUpdateStart={onEdgeUpdateStart}
-//         onEdgeUpdateEnd={onEdgeUpdateEnd}
-//         connectionLineType={ConnectionLineType.SmoothStep}
-//         fitView
-//       />
-//     </div>
-//   );
-// };
+  const onEdgeUpdate = useCallback((oldEdge, newConnection) => {
+    edgeUpdateSuccessful.current = true;
+    setEdges((els) => updateEdge(oldEdge, newConnection, els));
+  }, []);
 
-// export default LayoutFlow;
+  function FloatingEdge({ id, source, target, markerEnd, style }) {
+    const sourceNode = useStore(
+      useCallback((store) => store.nodeInternals.get(source), [source])
+    );
+    const targetNode = useStore(
+      useCallback((store) => store.nodeInternals.get(target), [target])
+    );
+    const foreignObjectSize = 40;
+    if (!sourceNode || !targetNode) {
+      return null;
+    }
+
+    const onEdgeClick = (evt, id) => {
+      evt.stopPropagation();
+      const edgeIndex = edges.findIndex((edge) => edge.id === id);
+      const updatedEdges = [...edges];
+      updatedEdges.splice(edgeIndex, 1);
+      setEdges(updatedEdges);
+    };
+
+    const { sx, sy, tx, ty } = getEdgeParams(sourceNode, targetNode);
+
+    const [edgePath, labelX, labelY] = getStraightPath({
+      sourceX: sx,
+      sourceY: sy,
+      targetX: tx,
+      targetY: ty
+    });
+
+    return (
+      <>
+        {' '}
+        <path
+          id={id}
+          className='react-flow__edge-path'
+          d={edgePath}
+          markerEnd={markerEnd}
+          style={style}
+        />
+        <foreignObject
+          width={foreignObjectSize}
+          height={foreignObjectSize}
+          x={labelX - foreignObjectSize / 2}
+          y={labelY - foreignObjectSize / 2}
+          className='edgebutton-foreignobject'
+          requiredExtensions='http://www.w3.org/1999/xhtml'
+        >
+          <div>
+            <button
+              className='edgebutton'
+              onClick={(event) => onEdgeClick(event, id)}
+            >
+              x
+            </button>
+          </div>
+        </foreignObject>
+      </>
+    );
+  }
+
+  const edgeTypes = {
+    floating: FloatingEdge
+  };
+
+  const onEdgeUpdateEnd = useCallback((_, edge) => {
+    if (!edgeUpdateSuccessful.current) {
+      setEdges((eds) => eds.filter((e) => e.id !== edge.id));
+    }
+
+    edgeUpdateSuccessful.current = true;
+  }, []);
+
+  return (
+    <div className='layoutflow' style={{ width: '100%', height: '100vh' }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+        nodeTypes={nodeTypes}
+        edgeTypes={edgeTypes}
+        defaultEdgeOptions={defaultEdgeOptions}
+        connectionLineComponent={CustomConnectionLine}
+        connectionLineStyle={connectionLineStyle}
+        snapToGrid
+        onEdgeUpdate={onEdgeUpdate}
+        onEdgeUpdateStart={onEdgeUpdateStart}
+        onEdgeUpdateEnd={onEdgeUpdateEnd}
+        connectionLineType={ConnectionLineType.SmoothStep}
+      />
+    </div>
+  );
+};
+
+export default EasyConnectExample;
--- src/easyconnect/FloatingEdje.js
+++
@@ -1,8 +0,0 @@
-// import { useCallback } from "react";
-// import { useStore, getStraightPath } from "reactflow";
-// import './style.css'
-// import { getEdgeParams } from "./utils.js";
-
-
-
-// export default FloatingEdge;
